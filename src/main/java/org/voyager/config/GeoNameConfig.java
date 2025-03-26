de208bd8972ec148bc67a1e36defc1c835723879
MaxineFonua <maxinefonua@gmail.com>
2025-03-25 22:31:54 -0700

    re-added app properties file w env vars for services

--- src/main/java/org/voyager/config/GeoNameConfig.java
+++ src/main/java/org/voyager/config/GeoNameConfig.java
@@ -2,6 +2,7 @@ package org.voyager.config;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -16,6 +17,7 @@ public class GeoNameConfig {
     String searchPath;
     String getPath;
     int maxRows;
+    @Value("${GEONAMES_API_USERNAME}")
     String username;
     Boolean isNameRequired;
     FeatureClass featureClass;
