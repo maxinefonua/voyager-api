95cb7f58892faeefa13431f30c6900f98a73c116
MaxineFonua <maxinefonua@gmail.com>
2026-04-18 20:30:41 -0700

    changes for paged countries

--- src/main/java/org/voyager/api/repository/primary/CountryRepository.java
+++ src/main/java/org/voyager/api/repository/primary/CountryRepository.java
@@ -1,11 +1,20 @@
 package org.voyager.api.repository.primary;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.voyager.commons.model.country.Continent;
 import org.voyager.api.model.entity.CountryEntity;
 import java.util.List;
 
 public interface CountryRepository extends JpaRepository<CountryEntity,String> {
     List<CountryEntity> findAllByOrderByNameAsc();
+    Page<CountryEntity> findAllByOrderByNameAsc(Pageable pageable);
+    @Query("SELECT c FROM CountryEntity c WHERE " +
+            "(:continentList IS NULL OR c.continent IN :continentList) " +
+            "ORDER BY c.name ASC")
+    Page<CountryEntity> findDynamic(@Param("continentList") List<Continent> continentList, Pageable pageable);
     List<CountryEntity> findByContinentInOrderByNameAsc(List<Continent> continentList);
 }
